4a8f8292a3f528e259930b05a79e4e5dc68a8b49
Andrii Sereda <a.a.sereda@ukr.net>
2023-01-03 13:41:41 +0200

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import { Routes, Route, NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+import { Movies } from './movies/Movies';
+import { Home } from './home/Home';
+
+const StyledLink = styled(NavLink)`
+  color: black;
+
+  &.active {
+    color: orange;
+  }
+`;
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <nav>
+        <StyledLink to="/">Home</StyledLink>
+        <StyledLink to="/movies">Products</StyledLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="*" element={<div>Not found</div>} />
+      </Routes>
     </div>
   );
 };
